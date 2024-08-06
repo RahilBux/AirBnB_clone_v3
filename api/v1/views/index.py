6561c84d0c417e69519ca282b4c12685016c88fa
RahilBux <rahilbux@gmail.com>
2024-08-06 02:35:23 +0200

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,20 @@ Views index
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
-    """Status of my API"""
+    """Status of API"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """Retrieve the num obj by type"""
+    stat = {}
+
+    for key, value in classes.items():
+        stat[key] = storage.count(value)
+
+    return jsonify(stat)
