a93066ff9b8e1e7b4d01a6222bf12af3e8fd0a20
RahilBux <rahilbux@gmail.com>
2024-08-06 02:37:42 +0200

    task fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,23 @@ Views index
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
+
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
+
 
 
 @app_views.route("/status")
